a0906483358a5b65e2e9ed4d54908d26836bd73d
samiakhlaq <mdakhlaq2345@gmail.com>
2024-04-04 14:49:52 +0600

    71A - Way Too Long Words

--- 71A - Way Too Long Words.py
+++ 71A - Way Too Long Words.py
@@ -2,7 +2,7 @@ def process(w):
     if len(w) <= 10:
         print(w)
     else:
-        w = w[0] + len(w)-2 + w[-1]
+        w = w[0] + str(len(w)-2) + w[-1]
         print(w)
 num_op = int(input())
 for x in range(num_op):
